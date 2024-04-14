894689524242262f10397224d0b73849992162cc
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-04-14 17:21:54 +0300

    small: add comments for example code

--- examples/client.cpp
+++ examples/client.cpp
@@ -2,13 +2,16 @@
 #include "synfilesharing/synfilesharing.h"
 
 int main() {
+    // В README.md мы создаём файлы "~/text_file.txt" и "~/markdown_file.md".
+    // Поскольку метод `sendFiles` требует абсолютного пути до файла
+    // мы конвертируем "~" в "/home/<user>".
     std::string pathToHome = std::getenv("HOME");
-
     std::vector<std::string> paths = {
             pathToHome + "/text_file.txt",
             pathToHome + "/markdown_file.md"
     };
 
+    // Создаём клиент и отправляем файлы.
     std::unique_ptr<synfs::IClient> client = synfs::makeClient().build();
     client->sendFiles(paths);
 
--- examples/server.cpp
+++ examples/server.cpp
@@ -4,20 +4,30 @@
 #include "synfilesharing/synfilesharing.h"
 
 int main(int argc, char *argv[]) {
+    // Создаём список разрешенных расширений файлов.
     std::vector<std::string> allowedFileExtensions = {".txt", ".md",};
 
+    // Создаём вектор, в который будут сохранены пришедшие от клиента файлы.
     auto receivedFiles = std::make_shared<std::vector<std::string>>();
+
+    // Создаём переменную, которая сообщит, был ли процесс запущен через DBus.
     bool runViaDBus = false;
 
+    // Создаём сервер (используя builder паттерн).
     std::unique_ptr<synfs::IServer> server = synfs::makeServer()
             .setAllowedFileExtensions(allowedFileExtensions)
             .saveRunViaDBusTo(runViaDBus)
+            .setExecFlag(synfs::constants::DEFAULT_EXEC_FLAG)
             .saveResultsTo(receivedFiles)
             .build();
 
+    // Если при запуске файла был указан `synfs::constants::DEFAULT_EXEC_FLAG`
+    // (или другой выбранный нами флаг), то это значит, что сервер был запущен
+    // силами DBus демона и должен принять файлы.
     server->run(argc, argv);
 
     if (runViaDBus) {
+        // Сервер был запущен через DBus. Распечатаем полученные файлы.
         for (const std::string &item: *receivedFiles) {
             std::cout << item;
             if (&item != &receivedFiles->back()) {
@@ -26,6 +36,7 @@ int main(int argc, char *argv[]) {
         }
         std::cout << '\n' << "СЕРВЕР: Я был запущен через DBus!" << '\n';
     } else {
+        // Сервер не был запущен через DBus.
         std::cout << '\n' << "СЕРВЕР: Я был запущен вне DBus!" << '\n';
         std::cout << "СЕРВЕР: Чтобы запустить меня так, как будто бы меня вызвал DBus, "
                      "добавьте флаг --launched-via-dbus" << '\n';
