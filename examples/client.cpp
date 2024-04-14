0fbaeb787daa88129d5b11fa8467169b32365d96
Nikita Krasnov <nikita.nikita.krasnov@gmail.com>
2024-04-14 16:54:43 +0300

    small: alter console messages in examples

--- examples/client.cpp
+++ examples/client.cpp
@@ -2,13 +2,15 @@
 #include "synfilesharing/synfilesharing.h"
 
 int main() {
+    std::string pathToHome = std::getenv("HOME");
+
     std::vector<std::string> paths = {
-            "/some/absolute/path/text_file.txt",
-            "/some/absolute/path/markdown_file.md"
+            pathToHome + "/text_file.txt",
+            pathToHome + "/markdown_file.md"
     };
 
     std::unique_ptr<synfs::IClient> client = synfs::makeClient().build();
     client->sendFiles(paths);
 
-    std::cout << "Привет, Мир!" << '\n';
+    std::cout << "КЛИЕНТ: Файлы успешно отправлены!" << '\n';
 }
